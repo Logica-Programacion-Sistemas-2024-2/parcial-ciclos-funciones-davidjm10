d1a060833e79cb66d9cfdd68afbb69afe9b0a541
davidjm10 <134171710+davidjm10@users.noreply.github.com>
2024-08-22 07:50:39 -0500

    Update App.java

--- app/src/main/java/parcial1g1/App.java
+++ app/src/main/java/parcial1g1/App.java
@@ -9,7 +9,7 @@
     
         public static void main(String[] args) {
             Scanner teclado = new Scanner(System.in);
-            float consumoPorKm = 0.08f;  // Consumo de combustible por kilómetro
+            float consumoPorKm = 0.08f;  
             float totalCombustible = 0;
     
             try {
